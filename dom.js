9a5bfe2ba4425a0b8271e49e77e9cc96519677f3
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-02-19 11:34:26 +0600

    adding dom

---
+++ dom.js
@@ -0,0 +1,22 @@
+document.getElementById("add-border").addEventListener("click", function(){
+    document.getElementById("friend-container").style.border = "3px solid black"
+})
+
+
+function changeBackground(){
+const friends = document.getElementsByClassName("friend");
+for(const friend of friends){
+    friend.style.backgroundColor = "lightBlue";
+}
+}
+
+document.getElementById("add-friend").addEventListener("click", function(){
+    const container = document.getElementById("friend-container");
+    const friendDiv = document.createElement("div");
+    friendDiv.classList.add("friend");
+    friendDiv.innerHTML = `
+    <h3 class = "friend-name">New Friend</h3>
+    <p>Quam, spaiente. </p>
+    `
+    container.appendChild(friendDiv);
+})
