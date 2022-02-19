82a2405bb926232c92a9963bf648c029d52696bb
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-02-19 12:27:40 +0600

    adding default parameter

---
+++ default-function-parameter.js
@@ -0,0 +1,18 @@
+//default can be set as below if there is no argument the default value will be taken. otherwise the default will be executed.
+function add(num1, num2 =0){
+
+    // before es6
+    //option-1
+    // num2 = num2 || 0;
+    //or
+
+    //option-2
+    //if(num2 == undefined){
+    //   num2 = 0;
+    //}
+    const total = num1 + num2;
+    return total;
+}
+
+const result = add (15, 17);
+console.log((result));
