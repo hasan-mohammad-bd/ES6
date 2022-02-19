2cb08061f0578adfc414cd4c8fc738531bfd590b
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-02-19 14:48:55 +0600

    adding arrow function

---
+++ arrow-function.js
@@ -0,0 +1,29 @@
+//normal function
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+//function in variable
+const add2 = function add2(num1, num2){
+    return num1 + num2;
+}
+//function expression (anonymous)
+//===========================================================
+//function in variable where function name dont mandatory.
+const add3 = function(num1, num2){
+    return num1 + num2;
+}
+
+//arrow function
+//===========================================================
+const add4 = (num1, num2) => num1 + num2;
+
+//the colling function will be same
+const sum1 = add(15, 17);
+const sum2 = add2(15, 17);
+const sum3 = add3(15, 17);
+const sum4 = add4(15, 17);
+console.log(sum1, sum2, sum3, sum4);
+// document.getElementById("my-btn").onClick = function haleEvent(){
+
+// }
